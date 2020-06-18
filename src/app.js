158c4bb08c0ecdfccedd7ef64248d32d4a1bef51
rudatorquato <rudatorquato@gmail.com>
2020-06-18 16:07:05 -0300

    mongoose

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const app = express();
 const router = express.Router();
+
+
+mongoose.connect('mongodb+srv://teste:123@cluster0-a4qwc.azure.mongodb.net/api?retryWrites=true&w=majority', 
+{ useNewUrlParser: true, useUnifiedTopology: true});
 
 // Carrega rotas
 const indexRoute = require ('./routes/index-route');
