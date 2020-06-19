1630162cd8c9342807e1fd1399d2029d17fbe522
rudatorquato <rudatorquato@gmail.com>
2020-06-19 13:08:28 -0300

    Model

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,7 +4,7 @@ exports.post = ('/', (req, res, next) =>{
     res.status(200).send(req.body);
 });
 
-exports.put = ('/', (req, res, next) =>{
+exports.put = ('/', (req, any, next) =>{
     const id = req.params.id;
     res.status(200).send({
         id: id,
