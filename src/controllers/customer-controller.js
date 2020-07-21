c454b9a55918f3d92c1ab0c512488c3009cf1989
rudatorquato <rudatorquato@gmail.com>
2020-07-21 14:27:56 -0300

    criptação de senha

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -4,6 +4,7 @@
 //const Customer = mongoose.model('Customer');
 const ValidationContract = require('../validators/fluent-validator');
 const repostory = require('../repositories/customer-repository');
+const md5 = require('md5')
 
 exports.post = async(req, res, next) =>{
     let contract = new ValidationContract();
@@ -18,7 +19,11 @@ exports.post = async(req, res, next) =>{
     }
 
     try{
-        await repostory.create(req.body)
+        await repostory.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
         res.status(200).send({
             message: 'Cliente cadastrado com sucesso!'
         });
